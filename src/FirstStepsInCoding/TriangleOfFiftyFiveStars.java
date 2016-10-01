8fe0f963e44a32c73f8b601e75d7aa9e3b74e5a3
Slav Petkov <slav.petev@gmail.com>
2016-10-01 18:42:58 +0300

    Solved problem TriangleOfFiftyFiveStars

---
+++ src/FirstStepsInCoding/TriangleOfFiftyFiveStars.java
@@ -0,0 +1,16 @@
+package FirstStepsInCoding;
+
+public class TriangleOfFiftyFiveStars {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println("********");
+        System.out.println("*********");
+        System.out.println("**********");
+    }
+}
