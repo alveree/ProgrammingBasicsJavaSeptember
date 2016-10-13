edb228314e1175107b1d4c1c4254596270403ed7
Slav Petkov <slav.petev@gmail.com>
2016-10-13 20:01:02 +0300

    Solved problem CircleAndPerimeter

---
+++ src/SimpleCalculations/CircleAreaAndPerimeter.java
@@ -0,0 +1,18 @@
+package SimpleCalculations;
+
+import java.util.Scanner;
+
+public class CircleAreaAndPerimeter {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        double radius = Double.parseDouble(
+                console.nextLine());
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.printf("Area = %f%sPerimeter = %f",
+                area, "\n", perimeter);
+    }
+}
