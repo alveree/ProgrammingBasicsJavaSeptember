e25ffff6909a249a9a6c9c9998294c870b1bdbd1
Slav Petkov <slav.petev@gmail.com>
2016-10-13 21:26:30 +0300

    Solved problem 1000 Days After Birth

---
+++ src/SimpleCalculations/ThousandDaysAfterBirth.java
@@ -0,0 +1,20 @@
+package SimpleCalculations;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ThousandDaysAfterBirth {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "dd-MM-yyyy");
+        LocalDate birthdate = LocalDate.parse(console.nextLine(),
+                formatter);
+
+        LocalDate thousandDaysAfterBirthdate = birthdate.plusDays(999);
+
+        System.out.println(formatter.format(thousandDaysAfterBirthdate));
+    }
+}
