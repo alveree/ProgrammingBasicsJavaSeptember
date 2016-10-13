717a3fba639e562410a5809dcb7cd4033249851c
Slav Petkov <slav.petev@gmail.com>
2016-10-13 19:56:19 +0300

    Solved problem TrapezoidArea

---
+++ src/SimpleCalculations/TrapezoidArea.java
@@ -0,0 +1,21 @@
+package SimpleCalculations;
+
+import java.util.Scanner;
+
+public class TrapezoidArea {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        double lowerBase = Double.parseDouble(
+                console.nextLine());
+        double upperBase = Double.parseDouble(
+                console.nextLine());
+        double height = Double.parseDouble(
+                console.nextLine());
+
+        double area = (upperBase + lowerBase) *
+                height / 2;
+
+        System.out.println(area);
+    }
+}
