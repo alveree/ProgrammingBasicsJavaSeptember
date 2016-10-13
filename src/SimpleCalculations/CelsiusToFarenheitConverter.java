f90d23e5bcaaeb66a16a107817cf73c244ef9cb0
Slav Petkov <slav.petev@gmail.com>
2016-10-13 20:31:27 +0300

    Solved problem CelsiusToFarenheit

---
+++ src/SimpleCalculations/CelsiusToFarenheitConverter.java
@@ -0,0 +1,17 @@
+package SimpleCalculations;
+
+import java.util.Scanner;
+
+public class CelsiusToFarenheitConverter {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        double celsiusTemperature = Double.parseDouble(
+                console.nextLine());
+
+        double farenheitTemperature = celsiusTemperature * 1.8 +
+                32;
+
+        System.out.printf("%.2f", farenheitTemperature);
+    }
+}
